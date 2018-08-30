ef5ef6b35ca413b2c84cf804a7a0ebe81871fc82
wordlu <1058744843@qq.com>
2018-08-30 20:53:58 +0800

    8.30changepage

--- pages/brand/info/info.js
+++ pages/brand/info/info.js
@@ -8,6 +8,21 @@ Page({
     brand:{
       logobg: "https://bm-mini.oss-cn-beijing.aliyuncs.com/demo/logo_bg.png",
       logourl:"https://bm-mini.oss-cn-beijing.aliyuncs.com/demo/logo%403x.png",
+    },
+    tab:0
+  },
+  tab_slide: function (e) {//滑动切换tab 
+    var that = this;
+    that.setData({ tab: e.detail.current });
+  },
+  tab_click: function (e) {//点击tab切换
+    var that = this;
+    if (that.data.tab === e.target.dataset.current) {
+      return false;
+    } else {
+      that.setData({
+        tab: e.target.dataset.current
+      })
     }
   },
 
